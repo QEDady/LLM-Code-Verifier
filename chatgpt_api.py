e7942b3574c05392f3b95a6a343c43eee99dd014
QEDady <amer.elsheikh@aucegypt.edu>
2024-03-19 23:23:40 +0200

    Refactored the API file into a function

--- chatgpt_api.py
+++ chatgpt_api.py
@@ -13,48 +13,66 @@ import requests
 import json
 
 
-
-URL = "https://api.openai.com/v1/chat/completions"
-api_key = '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'
-
-payload = {
-"model" : "gpt-4-turbo-preview",
-"messages":[
-    {"role": "system", "content": "You are a programming assistant, skilled in writing complex programming concepts with creative syntax."},
-    {"role": "user", "content": " Write a Python function in markdown that takes a sequence of numbers and determines whether all the numbers are different from each other. Return the code of the function only without any other text."}],
-"temperature" : 1.0,
-"top_p":1.0,
-"n" : 5,
-"stream": False,
-"presence_penalty":0,
-"frequency_penalty":0,
-}
-
-headers = {
-"Content-Type": "application/json",
-"Authorization": f"Bearer {api_key}"
-}
-
-response = requests.post(URL, headers=headers, json=payload, stream=False)
-
-text = response.content.strip()
-
-response_str_utf8 = text.decode("utf-8")
-
-response_dict = json.loads(response_str_utf8)
-
-total = 0
-codes=[]
-for choice in response_dict['choices']:
+def parse_response(choice):
   sentence = choice['message']['content'].split()
-  total += len(sentence)
   code = choice['message']['content'].replace('`',"")
   code = code.replace("python","")
-  codes.append(code)
+  return code
 
-for code in codes:
-  print(code)
+
+
+# n: number of samples to generate other than the refrence response
+# The function returns an array of n+1 codes where the first the refrence code and the others are the candidates. 
+def generate_codes(model="gpt-4-turbo-preview", n=5, t_refrence = 0, t_samples = 1, prompt=None):
+  URL = "https://api.openai.com/v1/chat/completions"
+  api_key = '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'
+  if prompt is None:
+    raise ValueError("prompt is not specifid")
+  
+  headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"Bearer {api_key}"
+  }
+
+  # refrence request
+  payload = {
+  "model" : model,
+  "messages":[
+      {"role": "system", "content": "You are a programming assistant, skilled in writing complex programming concepts with creative syntax."},
+      {"role": "user", "content": prompt}],
+  "temperature" : t_refrence,
+  "top_p":1.0,
+  "n" : 1,
+  "stream": False,
+  "presence_penalty":0,
+  "frequency_penalty":0,
+  }
+
+  refrence_response = requests.post(URL, headers=headers, json=payload, stream=False).content.strip().decode("utf-8")
+  response_dict = json.loads(refrence_response)
+  generated_codes=[]
+  for choice in response_dict['choices']:
+    generated_codes.append(parse_response(choice))
+
+  print(response_dict)
   print("---------------------------------------------------")
+  print(generated_codes)
 
-print(response_dict)
 
+  # sampled responses
+  payload["temperature"] = t_samples
+  payload["n"] = n
+  refrence_response = requests.post(URL, headers=headers, json=payload, stream=False).content.strip().decode("utf-8")
+  response_dict = json.loads(refrence_response)
+  for choice in response_dict['choices']:
+    generated_codes.append(parse_response(choice))
+
+  print(response_dict)
+  print("---------------------------------------------------")
+  print(generated_codes)
+  return generate_codes
+
+
+# if __name__=="__main__": 
+#   prompt = " Write a Python function in markdown that takes a sequence of numbers and determines whether all the numbers are different from each other. Return the code of the function only without any other text."
+#   generate_codes(prompt=prompt)
