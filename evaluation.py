981e34c2761124bac59b831dfbf036efded2ad96
amrsallam10 <amrmohsallam@aucegypt.edu>
2024-03-28 23:20:03 +0200

    HumanEval tests modification

--- evaluation.py
+++ evaluation.py
@@ -3,19 +3,48 @@ import subprocess
 import tqdm
 from chatgpt_api import generate_codes
 from data import stream_jsonl, HUMAN_EVAL
+import re
 
-for problem in stream_jsonl(HUMAN_EVAL):
-    prompt = problem['prompt']
-    codes = generate_codes(prompt=prompt)
-    for code in codes:
-        check_program = f"{problem['prompt']}{code}\n{problem['test']}\ncheck({problem['entry_point']})"
-        try:
-            result = subprocess.run(['python3', '-c', check_program], stderr=subprocess.PIPE)
-            if result.returncode == 0:
-                print(f"[{problem['task_id']}] Correct")
-            else:
-                print(f"[{problem['task_id']}] Incorrect")
-                err = result.stderr.decode('utf-8')
-                # print('Error:', err)
-        except Exception as e:
-            print('Error:', e)
+def eval_Human_eval():
+    err_num = 0
+    for problem in stream_jsonl(HUMAN_EVAL):
+        # if problem['task_id'] == 'HumanEval/66':
+            # print(problem['test'])
+            problem['test'] = modify_test(problem['test'])
+            prompt = problem['prompt']
+            # codes = generate_codes(prompt=prompt)
+            codes = [problem['canonical_solution']]
+            for code in codes:
+                check_program = f"{problem['prompt']}{code}\n{problem['test']}\nprint(check({problem['entry_point']}))"
+                # print(check_program)
+                result = subprocess.run(['python3', '-c', check_program], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                if result.returncode != 0:
+                    print(f"[{problem['task_id']}] Error {result.stderr.decode()}")
+                    err_num += 1
+                    continue
+                print(f"[{problem['task_id']}] Pass rate: ", result.stdout.decode())
+    print(f"Total error: {err_num}")
+
+
+
+def modify_test(test):
+    pattern = r"assert"
+    matches = re.findall(pattern, test)
+    insertion = f"""
+    total_tests = {len(matches)}
+    passed_tests = 0
+    """
+
+    pattern = r"(def check\(candidate\):)"
+    replacement = r"\1" + insertion
+    test = re.sub(pattern, replacement, test)
+    pattern = r'assert\s+(.+?)\s+(==|!=|>|<|>=|<=)\s+(.+?)$'
+
+    test = re.sub(pattern, lambda match: f"if {match.group(1)} {match.group(2)} {match.group(3)}:\n            passed_tests += 1", test, flags=re.MULTILINE)
+
+    # Add a return statement for the pass rate
+    test += "\n    return passed_tests / total_tests"
+    return test
+
+if __name__ == '__main__':
+    eval_Human_eval()
