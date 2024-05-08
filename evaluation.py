5ad98c15ca064f61ebc707c8c8ecbd9ff4ed6fc4
amrsallam10 <amrmohsallam@aucegypt.edu>
2024-05-08 05:37:09 +0300

    run modified tests on same codes of trial 1

--- evaluation.py
+++ evaluation.py
@@ -83,6 +83,35 @@ def eval_Human_eval(model="gpt-3.5-turbo", n=5, t_refrence=0, t_samples=1, trial
                     row[f"pass_rate_{i}"] = pass_rate*100
                 writer.writerow(row)
 
+def eval_Human_eval_from_file(model="gpt-3.5-turbo", n=5, t_refrence=0, t_samples=1, trial=1):
+    csv_file_name, fieldnames, last_task_id_num = create_csv_file(dataset="HumanEval", model=model, n=n, 
+                                                 t_refrence=t_refrence, t_samples=t_samples, trial=trial)
+    
+    codes_file_name, _, _ = create_csv_file(dataset="HumanEval", model=model, n=n, 
+                                                 t_refrence=t_refrence, t_samples=t_samples, trial=1)
+    pass_rate = 0
+    err = None
+    row = {}
+
+    with open(csv_file_name, mode='a', newline='') as csv_f, open(codes_file_name, mode = 'r') as codes_f:
+        writer = csv.DictWriter(csv_f, fieldnames=fieldnames, quoting=csv.QUOTE_ALL) 
+        reader = csv.DictReader(codes_f, quoting=csv.QUOTE_ALL)
+        for problem in stream_jsonl(HUMAN_EVAL_MODIFIED):        
+            read_row = next(reader)
+            if int(problem['task_id'].split('/')[-1]) > last_task_id_num:
+                print(f"Processing {problem['task_id']}")
+                row['task_id'] = problem['task_id']
+                row['prompt'] = problem['prompt']
+                for i in range (n+1):
+                    code = read_row[f"code_{i}"]
+                    row[f"code_{i}"] = code
+                    check_program = f"{code}\n{problem['test']}\nprint(check({problem['entry_point']}))"
+                    err, pass_rate = run_test_HUMAN_EVAL(check_program)
+                    row[f"err_{i}"] = err
+                    row[f"pass_rate_{i}"] = pass_rate*100
+                writer.writerow(row)
+
+
 def eval_APPS(args, model="gpt-3.5-turbo", n=5, t_refrence=0, t_samples=1, trial=1):
     csv_file_name, fieldnames, last_task_id_num = create_csv_file(dataset="APPS", model=model, n=n, 
                                                  t_refrence=t_refrence, t_samples=t_samples, trial=trial)
@@ -240,7 +269,7 @@ def read_test(dataset, idx):
             problem = json.loads(line)
             problem_id = int(problem['task_id'].split('/')[-1])
             if problem_id == idx:
-                print(f"{problem['test']}")
+                pprint.pprint(problem['test'])
                 break
 
 def create_parser():
@@ -267,21 +296,21 @@ if __name__ == '__main__':
     # loop = asyncio.get_event_loop()
     # loop.run_until_complete(eval_Human_eval_async(model='gpt-3.5-turbo', n=5, t_refrence=0, t_samples=1, trial=1))
     trial = input("Press Enter trial number: ")
-    eval_APPS(args, model='gpt-3.5-turbo', n=5, t_refrence=0, t_samples=1, trial=trial)
+    # eval_APPS(args, model='gpt-3.5-turbo', n=5, t_refrence=0, t_samples=1, trial=trial)
 
-    # eval_Human_eval(model='gpt-3.5-turbo', n=5, t_refrence=0, t_samples=1, trial=trial)
-    # eval_Human_eval(model='gpt-3.5-turbo', n=3, t_refrence=0, t_samples=1, trial=trial)
-    # eval_Human_eval(model='gpt-3.5-turbo', n=10, t_refrence=0, t_samples=1, trial=trial)
-    # eval_Human_eval(model='gpt-3.5-turbo', n=5, t_refrence=1, t_samples=1, trial=trial)
-    # eval_Human_eval(model='gpt-3.5-turbo', n=5, t_refrence=0, t_samples=1.5, trial=trial)
-    # eval_Human_eval(model='gpt-3.5-turbo', n=15, t_refrence=0, t_samples=1, trial=trial)
-    # eval_Human_eval(model='gpt-3.5-turbo', n=15, t_refrence=1, t_samples=1, trial=trial)
-    # eval_Human_eval(model='gpt-3.5-turbo', n=15, t_refrence=1, t_samples=1.3, trial=trial)
+    # eval_Human_eval_from_file(model='gpt-3.5-turbo', n=5, t_refrence=0, t_samples=1, trial=trial)
+    # eval_Human_eval_from_file(model='gpt-3.5-turbo', n=3, t_refrence=0, t_samples=1, trial=trial)
+    # eval_Human_eval_from_file(model='gpt-3.5-turbo', n=10, t_refrence=0, t_samples=1, trial=trial)
+    # eval_Human_eval_from_file(model='gpt-3.5-turbo', n=5, t_refrence=1, t_samples=1, trial=trial)
+    # eval_Human_eval_from_file(model='gpt-3.5-turbo', n=5, t_refrence=0, t_samples=1.5, trial=trial)
+    # eval_Human_eval_from_file(model='gpt-3.5-turbo', n=15, t_refrence=0, t_samples=1, trial=trial)
+    # eval_Human_eval_from_file(model='gpt-3.5-turbo', n=15, t_refrence=1, t_samples=1, trial=trial)
+    # eval_Human_eval_from_file(model='gpt-3.5-turbo', n=15, t_refrence=1, t_samples=1.3, trial=trial)
 
-    # eval_Human_eval(model='gpt-4-turbo-preview', n=5, t_refrence=0, t_samples=1, trial=1)
+    eval_Human_eval_from_file(model='gpt-4-turbo-preview', n=5, t_refrence=0, t_samples=1, trial=trial)
     # eval_Human_eval(model='gpt-4-turbo-preview', n=3, t_refrence=0, t_samples=1, trial=1)
     # eval_Human_eval(model='gpt-4-turbo-preview', n=10, t_refrence=0, t_samples=1, trial=1)
     # eval_Human_eval(model='gpt-4-turbo-preview', n=5, t_refrence=1, t_samples=1, trial=1)
     # eval_Human_eval(model='gpt-4-turbo-preview', n=5, t_refrence=0, t_samples=1.5, trial=1)
-    # read_test("APPS", 1)
+    # read_test("APPS", 30)
     
